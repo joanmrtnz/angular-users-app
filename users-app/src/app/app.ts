9e76f57b94290d14c8c110105dbf7210a57c6b1a
xuanor <joancomic.jm@gmail.com>
2025-06-21 18:26:23 +0200

    feat(header): add links in header nav

--- users-app/src/app/app.ts
+++ users-app/src/app/app.ts
@@ -7,11 +7,21 @@ import {RouterModule} from '@angular/router';
   imports: [Home, RouterModule],
   template: `
     <main>
+      
+      <header>
       <a [routerLink]="['/']">
-        <header class="brand-name">
           <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
-        </header>
       </a>
+      <nav>
+        <a [routerLink]="['/']">
+          <p>Users List</p>
+        </a>
+        <a [routerLink]="['/new-user']">
+          <p>New User</p>
+        </a>
+        
+      </nav>  
+      </header>
       <section class="content">
         <router-outlet></router-outlet>
       </section>
